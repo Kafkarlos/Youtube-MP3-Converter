b86aca2c988b231a82b450477c43ce3f7627da2a
not2nder <brtiodark@gmail.com>
2026-06-01 14:13:02 -0300

    Feat: Qualidade informada via command line

--- main.py
+++ main.py
@@ -21,39 +21,13 @@ def download(url, quality):
 
     print("Download Concluído!")
 
-def main():
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('url', nargs="?", type=str)
-    args = parser.parse_args()
-    
-    if not args.url:
-        print("[bold white on red] Buscar Musica [/]")
-
-        url = inquirer.text(
-            message="URL:",
-        ).execute()
-
-    else:
-        url = args.url
-
-    get_info(url)
-
-    quality = inquirer.select(
-        message="Escolha a qualidade (Kbps)",
-        choices= [128,192,320],
-        vi_mode=True
-    ).execute()
-
-    download(url, quality)
-
 def get_duration(secs: int):
     mins = secs//60
     rest = str(secs%60).rjust(2,"0")
 
     return f"{mins}:{rest}"
 
-def get_info(url):
+def show_info(url):
     opts = {
         "quiet": True
     }
@@ -62,12 +36,43 @@ def get_info(url):
         info = ydl.extract_info(url, download=False)
 
         print(f"Título: {info.get('title')}")
+        print(f"Canal: {info.get('channel')}")
         
         duration = info.get('duration')
         if duration:
             print(f"Duração: {get_duration(info.get('duration'))}")
-        
-        print(f"Canal: {info.get('channel')}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('url', nargs="?", type=str)
+    parser.add_argument(
+        "--quality",
+        help="Qualidade do áudio (kbps)",
+        choices=[128,192,320],
+        type=int
+    )
+
+    args = parser.parse_args()
+    
+    url = inquirer.text(
+        message="URL:",
+    ).execute() if not args.url else args.url
+
+    show_info(url)
+
+    confirm = input("Prosseguir com o Download? [S/n]:").lower()
+    
+    if not confirm or confirm == "s":
+        quality = inquirer.select(
+            message="Escolha a qualidade (Kbps)",
+            choices= [128,192,320],
+            vi_mode=True
+        ).execute() if not args.quality else args.quality
+
+        download(url, quality)
+    else:
+        return
 
 if __name__ == '__main__':
     main()
